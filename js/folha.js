c5f256a47e7ecb47a81e5f9a3d4af0e7a5469e22
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-02-14 01:03:29 -0300

    readme
    readme

--- js/folha.js
+++ js/folha.js
@@ -1,3 +1,57 @@
-function envento(){
-    
+ 
+ //CARROSSEL DE IMAGENS NA PÁGINA DE EVENTOS
+ const imagem = document.getElementById('img')
+ const img =  document.querySelectorAll('#img img')
+
+ let car_img = 0
+
+ function carrossel(){
+     car_img++
+
+     if (car_img > img.length - 1){
+         car_img = 0
+     }
+
+     imagem.style.transform = `translateX(${-car_img * 100}%)`
+
+ }
+
+ setInterval (carrossel, 2500)
+
+
+ //GRÁFICO DA PASTA RELATÓRIO
+const rg = document.getElementById('meu_grafico').getContext("2d")
+
+const labels =[
+    '1º semana',
+    '2º semana',
+    '3º semana',
+    '4º semana',
+    '5º semana',
+    '6º semana',
+    '7º semana',
+    '8º semana',
+    '8º semana',
+    '10º semana'
+]
+
+const data = {
+    labels,
+    datasets: [{
+        data: [10,25,35,40,60,65,75,90,95,100 ],
+        label: "Progressão do projeto Ideia2022",
+        fill:true
+    }]
 }
+
+const config = {
+    type:'line',
+    data,
+    options: {
+        responsive: true,
+        radius: 4,
+        hoverRadius: 7
+    }
+}
+
+const myChart = new Chart(rg, config)
